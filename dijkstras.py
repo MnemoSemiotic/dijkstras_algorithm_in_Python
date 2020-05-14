4966a22066e0c162b4fcafe8b49d3eda1fe817b6
Andrew Roberts <clownfragment@gmail.com>
2020-05-14 15:58:46 -0600

    working cost table

--- dijkstras.py
+++ dijkstras.py
@@ -1,4 +1,6 @@
 
+class Graph: 
+
 
 class Node:
     def __init__(self, nodename, connections):
@@ -10,13 +12,26 @@ class Node:
         for name, cost in self.connections.items():
             self.cost_table[name] = {'cost': cost, 'previous': self.nodename}
 
-    # def __repr__(self):
+    def __repr__(self):
+        cost_table_header = '\tNode\tCost\tPrevious\n'
+        cost_table_str = ''.join([f'\t{node}\t{vals["cost"]}\t{vals["previous"]}\n' for node, vals in self.cost_table.items()])
 
+        return f'nodename: {self.nodename}\nconnections: {self.connections}\ncost_table:\n {cost_table_header}{cost_table_str}'
 
+    def __str__(self):
+        cost_table_header = '\tNode\tCost\tPrevious\n'
+        cost_table_str = ''.join([f'\t{node}\t{vals["cost"]}\t{vals["previous"]}\n' for node, vals in self.cost_table.items()])
 
+        return f'nodename: {self.nodename}\nconnections: {self.connections}\ncost_table:\n {cost_table_header}{cost_table_str}'
+
+    def build_cost_table(self, graph):
+        pass
 
 if __name__ == '__main__':
     # define nodes
     A = Node('A', {'B': 5, 'C': 7, 'D': 2})
 
-    print(A)
+    # print(A)
+
+    graph = Graph()
+    graph.add_nodes([A])
